c6673ef5a796a8b197d250c1c5dc14d184bb6153
Edvard Pedersen <edvard.pedersen@uit.no>
2021-02-02 15:35:40 +0100

    fix issue with particles

--- particles.py
+++ particles.py
@@ -5,26 +5,20 @@ SCREEN_X = 800
 SCREEN_Y = 800
 PARTICLE_IMAGE = "particle.png"
 PARTICLES_PER_SECOND = 20
-GRAVITY = 9.81
+GRAVITY = 598.1
 
 class Particle:
-    def __init__(self, position, direction, speed):
+    def __init__(self, position, direction):
         self.position = pygame.math.Vector2(position)
-        self.speed = speed
-        self.direction = pygame.math.Vector2(direction).normalize()
-
-    def load_image(self, image_filename):
-        return pygame.image.load(image_filename).convert_alpha()
+        self.direction = pygame.math.Vector2(direction)
+        self.image = pygame.image.load(PARTICLE_IMAGE).convert_alpha()
 
     def draw(self, screen):
-        self.image = self.load_image(PARTICLE_IMAGE)
         screen.blit(self.image, self.position)
 
     def move(self, time_passed):
-        self.position += (self.direction * self.speed) * time_passed
+        self.position += self.direction * time_passed
         self.direction.y += (GRAVITY  * time_passed)
-        self.speed = self.speed * self.direction.length()
-        self.direction = self.direction.normalize()
 
 if __name__ == "__main__":
     pygame.init()
@@ -41,14 +35,16 @@ if __name__ == "__main__":
             elif event.type == pygame.USEREVENT:
                 print("{} frames last second, with {} particles active".format(frame_counter, len(particles)))
                 frame_counter = 0
+            elif event.type == pygame.KEYDOWN:
+                import pdb; pdb.set_trace()
         frame_counter += 1
         time_passed = clock.tick(200) / 1000.0
 
         for i in range(int(2000 * time_passed)):
             particles.append(Particle(
                             (SCREEN_X / 2, SCREEN_Y / 2), 
-                            (random.randint(-100, 100), -100), 
-                            random.random()*3000.0))
+                            (random.randint(-100, 100), 
+                            -random.random()*700.0)))
 
         screen.fill((0,0,0))
         for particle in particles:
